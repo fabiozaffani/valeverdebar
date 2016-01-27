a8bfbfdb62dc879691c7e436ee75e230c64e4593
fabiozaffani@gmail.com <fabiozaffani@gmail.com>
2016-01-27 13:15:29 -0200

    Updated calculator values

--- includes/calculator.php
+++ includes/calculator.php
@@ -5,9 +5,9 @@ final class Calculator {
 	public function __construct() {
         $this->startValue = 1500;
         // valor por pessoa de sabado
-        $this->personSaturday = 6;
+        $this->personSaturday = 5.6;
         // valor por pessoa de final de semana
-        $this->personWeekend = 4;
+        $this->personWeekend = 3.8;
         // valor por pessoa durante a semana
         $this->personWeek = 2;
         // valor de locação por pessoa para mesa e cadeira extra
